513873fc28952aa2b1aa9a24997f3ca213da6377
H452433 <Mohammed.Alam@Honeywell.com>
2021-10-27 15:19:53 +0530

    Implemented Logic to Find odd number in array

---
+++ Practice_Code/Practice_Code/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice_Code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 12 };
+            ArrayList arrayList = new ArrayList();
+            int result = arrayList.sumAllOdds(array);
+            Console.WriteLine("Print Sum Of All odd number is:" + " " + result);
+
+            Console.ReadLine();
+        }
+
+        public class ArrayList
+        {
+            public int sumAllOdds(int[] array)
+            {
+                int sum = 0;
+                //int[] arr = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+                for (int i = 0; i < array.Length; i++)
+                {
+                    if (array[i] % 2 != 0)
+                    {
+                        sum += array[i];
+                    }
+
+                }
+                return sum;
+
+            }
+        }
+    }
+    
+}
